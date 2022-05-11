be41db513ac51f475cd8a84c0d8a4396be1d23ca
minhnhat2904 <phancaominhnhat10@gmail.com>
2022-05-11 11:33:48 +0700

    make it better

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,10 +3,6 @@ import { adminController } from './admin.controller';
 import { cvController } from './cv.controller';
 import { followController } from './follow.controller';
 import { permissionController } from './permission.controller';
-import authController from "./auth.controller";
-import { adminController } from "./admin.controller";
-import { cvController } from "./cv.controller";
-import { followController } from "./follow.controller";
 import { iterController } from "./iter.controller";
 import { postController } from "./post.controller";
 
